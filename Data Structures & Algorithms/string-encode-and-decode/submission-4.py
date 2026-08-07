958f3dd0fee414c3aaecb3f38f8ca2f75687cbc0
Mansa Vivek Ganore <mansaganore98@gmail.com>
2026-08-07 13:29:20 -0400

    Add: string-encode-and-decode - submission-4

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-4.py
@@ -0,0 +1,15 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        # for str in strs:
+        if strs == []:
+            return "emptyObject"
+        encodedStr = "~".join(strs)
+        return encodedStr
+
+    def decode(self, s: str) -> List[str]:
+        decodedStrs = s.split('~')
+        if decodedStrs[0] == "emptyObject":
+            print("mansa")
+            return []
+        return decodedStrs
